6ea347fde634263ef3f20abf3486340786a24d71
Yara Rashed <yara.rashed1@hotmail.com>
2022-06-27 14:36:30 +0300

    DailyChallenge

---
+++ Week2/Day2/DailyChallenge/script.js
@@ -0,0 +1,19 @@
+let sentence = "This dinner is not that bad ! You cook well";
+
+let wordNot = Number(sentence.indexOf("not"));
+//console.log(wordNot);
+
+let wordBad = Number(sentence.indexOf("bad"));
+//console.log(wordBad);
+
+
+if(wordBad > wordNot && wordNot != -1){
+	let x = sentence.slice(0,wordNot);
+	let y = sentence.slice(wordBad+3);
+	let newsentence = x + " good " + y;
+	console.log(newsentence);
+	}
+else {
+	console.log(sentence);
+}
+	

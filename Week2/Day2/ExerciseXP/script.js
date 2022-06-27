4aa19768158e6a0e012e174726071ad37aead38d
Yara Rashed <yara.rashed1@hotmail.com>
2022-06-27 12:50:47 +0300

    Week2Day2ExerciseXP

--- Week2/Day2/ExerciseXP/script.js
+++ Week2/Day2/ExerciseXP/script.js
@@ -35,6 +35,23 @@ else console.log("x is an odd number");
 let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
 console.log(users.length);
 
+if(users.length == 0)
+{
+	console.log("no one is online");
+}
+else if(users.length == 1){
+	console.log(users[0] + " is online");
+}
+else if(users.length == 2){
+	console.log(users[0] +" and " + users[1] + " are online");
+}
+
+else {
+	console.log(users[0] + " and " + users[1] + " and " + (users.length-2) + " more are online");
+
+}
+
+
 
 
 

cf4072b029da0da1f1f6d247ae974fe18f8f87c8
Yara Rashed <yara.rashed1@hotmail.com>
2022-07-19 15:29:24 +0300

    XP

--- Week5/Day3/ExerciseXP/script.js
+++ Week5/Day3/ExerciseXP/script.js
@@ -1,66 +1,66 @@
-// //EX1 
+//EX1 
 
-// function compareToTen(num){
-//     let isTen = new Promise((resolve,reject) => {
-//         if(num<10){
-//             resolve("resolved");
-//         }else{
+function compareToTen(num){
+    let isTen = new Promise((resolve,reject) => {
+        if(num<10){
+            resolve("resolved");
+        }else{
             
-//             reject("rejected");
-//         }
-//     })
-//     return isTen;
-// };
+            reject("rejected");
+        }
+    })
+    return isTen;
+};
 
 
-// let p1 = (compareToTen(15));
+let p1 = (compareToTen(15));
 
-// p1
-// .then(result => console.log(result))
-// .catch(error => console.log(error));
+p1
+.then(result => console.log(result))
+.catch(error => console.log(error));
   
-// let p2 = compareToTen(8);
-// p2
-//   .then(result => console.log(result))
-//   .catch(error => console.log(error));
+let p2 = compareToTen(8);
+p2
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
 
 
 
 
-// //EX2 
+//EX2 
 
-// // 1 
+// 1 
 
 
 
-// setTimeout(()=>{
-//     let p = new Promise((resolve,reject)=>{
-//         if(true){
-//             // console.log("success");
-//             resolve("success")
-//         }else{
-//             reject("unsuccessful")
-//         }
-//     })
-//     .then(res=>{console.log(res)});
-// },4000);
+setTimeout(()=>{
+    let p = new Promise((resolve,reject)=>{
+        if(true){
+            // console.log("success");
+            resolve("success")
+        }else{
+            reject("unsuccessful")
+        }
+    })
+    .then(res=>{console.log(res)});
+},4000);
 
-// //2  //3 
-// //using the promise.resolve can have it done faster
+//2  //3 
+//using the promise.resolve can have it done faster
 
-// let p = Promise.resolve("success"); //when success is changed it throws the error
-// p.then(res =>{
-//     if(res === "success"){
-//     console.log(res);
-//     }
-//     else{
-//     throw new Error("error");
-//     }
+let p = Promise.resolve("success"); //when success is changed it throws the error
+p.then(res =>{
+    if(res === "success"){
+    console.log(res);
+    }
+    else{
+    throw new Error("error");
+    }
 
-// })
-// p.catch(error=>{
-//     console.log("Ooops something went wrong");
-// })
+})
+p.catch(error=>{
+    console.log("Ooops something went wrong");
+})
 
 
 //EX 3 

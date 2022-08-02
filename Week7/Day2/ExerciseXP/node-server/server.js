449b1a4064bd76f04f38aa5d063fa82b5efed3aa
Yara Rashed <yara.rashed1@hotmail.com>
2022-08-02 13:29:50 +0300

    EX_XP

---
+++ Week7/Day2/ExerciseXP/node-server/server.js
@@ -0,0 +1,29 @@
+//EX1
+
+// const http = require('http');
+
+// const server = http.createServer((req,res)=>{
+//     res.end(`<h1> This is my first response </h1> <h2> This is my second response </h2> <h3> This is my third response </h3>`);
+// })
+
+// server.listen(3000, ()=>{console.log("I'm listening")});
+
+
+//EX2
+
+
+// const a = require('../script.js');
+// let b = JSON.stringify(a.user);
+// const http = require('http');
+
+// const server = http.createServer((req,res)=>{
+//     res.end(b);
+// })
+
+// server.listen(8080, ()=>{console.log("I'm listening")});
+
+//EX3 
+
+
+
+

49c1b9cdb93100f3b137e01f315478321df62a9e
Yara Rashed <yara.rashed1@hotmail.com>
2022-08-02 08:20:18 +0300

    XP

---
+++ Week7/Day1/ExerciseXP/npm-node/main.js
@@ -0,0 +1,25 @@
+// //****** EX 1 ****** */
+
+
+// let a = 5;
+// let b = 10;
+
+// console.log("Hello you are" , a+b);
+ 
+// //run in terminal -> node main.js
+
+
+// //****** EX 2 ****** */
+//  console.log("Hi, thanks for running me with npm start!");
+
+//****** EX 3 ****** */
+let a = 5;
+let b = 10;
+let x;
+let c = 6;
+x = a + b;
+
+console.log(`First run: ${a+b} `);
+
+console.log(`First run: ${x+c} `);
+
